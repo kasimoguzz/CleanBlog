e4baabf6752534f18c488e0ade04af61b873b617
muhammed kasim oguz <muhammedkasimoguz@gmail.com>
2023-05-18 18:19:24 +0300

    CleanBlog | Tepmlate

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 
-
+const ejs = require('ejs');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+//Template Engine
+  app.set("view engine", "ejs");
 
-    const blog = { 
-        id: 1,
-        title: "Blog title",
-        description: "Blog description" }
-        res.send(blog)
-})
+  app.use(express.static('public'));
+
+  app.get("/", (req, res) => {
+    res.render('index');
+  });
+
+  app.get("/about", (req, res) => {
+    res.render('about');
+  });
+
+  app.get("/add_post", (req, res) => {
+    res.render('add_post');
+  });
+
+  app.get("/post", (req, res) => {
+    res.render('post');
+  });
+
 
 const port = 3000;
 app.listen(port, () => {
